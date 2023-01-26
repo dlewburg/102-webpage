acc0d02a3a6f2bdf335ef8b5e33bad51dbe12ceb
Dasha Burgos <dasha.lewis@gmail.com>
2023-01-25 20:07:35 -0500

    Errors cause code to function differently in different browser

--- script.js
+++ script.js
@@ -11,16 +11,16 @@ document.write("  It's always a good time for a Milkshake!  ");
 let shakePref = prompt ("Do you prefer Classic or Fancy Milkshakes?");
 
 if(shakePref == "classic"){
-   let element=document.getElementById(classic)
-   element.scrollIntoView({behavior: 'smooth'});
+   //let element=document.getElementById(classic)
+  // element.scrollIntoView({behavior: 'smooth'});
    document.write("  Classic Shakes are the BEST!  ");
    
 }else if(shakePref == "fancy"){
-    let fancyElement=document.getElementById(fancy)
-    fancyElement.scrollIntoView({behavior: 'smooth'});
+    //let fancyElement=document.getElementById(fancy)
+   // fancyElement.scrollIntoView({behavior: 'smooth'});
     document.write("  Fancy Shakes SOOTH the SOUL!  ");
 }else{
-    let headerElement=document.getElementById(header)
-    headerElement.scrollIntoView({behavior: 'smooth'});
+    //let headedElement=document.getElementById(headed)
+    //headedElement.scrollIntoView({behavior: 'smooth'});
     document.write("  Not sure what type of shake you prefer. Refresh the page to try again!")
 };
