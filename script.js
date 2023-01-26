0afacface5a082d13b2eda5ca9bcef8c4f1aa05c
Dasha Burgos <dasha.lewis@gmail.com>
2023-01-26 17:49:41 -0500

    created functions for conditional statements

--- script.js
+++ script.js
@@ -1,26 +1,45 @@
 let visitorName = prompt ("What is your name?");
 
-if (confirm("Do you WANT to visit this Milkshake site?")){
+function checkName(visitorName){
+    if(visitorName === ""){  
+        alert("Whoa! You didn't input anything!");
+        visitorName = prompt("May I have a your name?"); 
+        return visitorName
+    }else if(visitorName == null){  
+        alert("Whoa! You don't have to cancel!");
+        visitorName = prompt("May I have a your name?");
+        return visitorName
+    }else{
+        return
+    };
+}
+
+function enterSite(){
+    if (confirm(visitorName + ", do you WANT to visit this Milkshake site?")){
     document.write("Welcome " + visitorName + "!  ");
 }else {
     document.write("You're Here Now! Welcome "+ visitorName + "!  ");
 };
+}
 
-document.write("  It's always a good time for a Milkshake!  ");
+function shakeType (){
+    
+    let shakePref = prompt ("Do you prefer Classic or Fancy Milkshakes?");
+    
+    shakePref = shakePref.toLowerCase();
 
-let shakePref = prompt ("Do you prefer Classic or Fancy Milkshakes?");
+    if(shakePref == "classic"){
+        document.write("  Classic Shakes are the BEST!  ");
+       
+    }else if(shakePref == "fancy"){
+        document.write("  Fancy Shakes SOOTH the SOUL!  ");
+    }else{
+        document.write("  Not sure what type of shake you prefer. Refresh the page to try again!")
+    
+    return shakePref 
+    };
+}
 
-if(shakePref == "classic"){
-   //let element=document.getElementById(classic)
-  // element.scrollIntoView({behavior: 'smooth'});
-   document.write("  Classic Shakes are the BEST!  ");
-   
-}else if(shakePref == "fancy"){
-    //let fancyElement=document.getElementById(fancy)
-   // fancyElement.scrollIntoView({behavior: 'smooth'});
-    document.write("  Fancy Shakes SOOTH the SOUL!  ");
-}else{
-    //let headedElement=document.getElementById(headed)
-    //headedElement.scrollIntoView({behavior: 'smooth'});
-    document.write("  Not sure what type of shake you prefer. Refresh the page to try again!")
-};
+checkName();
+enterSite();
+shakeType();
